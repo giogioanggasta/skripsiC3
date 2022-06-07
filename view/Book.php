776fe83ff036b66a6dcde68a70962fc4ad3eb783
giogioanggasta <giogioanggasta@gmail.com>
2022-06-07 20:12:03 +0700

    nambahin data dan sendmail

--- view/Book.php
+++ view/Book.php
@@ -118,17 +118,7 @@
 <body>
 
 
-<div class="w3-bar w3-white w3-border" id="menu">
-    <a href="Home-logged.php" class="w3-bar-item"><img src="../images/logoc3.png" style="width:150px"></a>
-    <a href="Home.php" class="w3-bar-item" style="float: right; margin-top:2%; text-decoration: none;"><img src="../images/logout.png" style="width:25px"></a>
-    <a href="Profile.php" class="w3-bar-item" style="float: right; margin-top:2%; text-decoration: none;"><img src="../images/user.png" style="width:30px"></a>
-    <a href="Membership.php" class="w3-bar-item" style="float: right; margin-top:2%; text-decoration: none;">Membership</a>
-    <a href="News.php" class="w3-bar-item" style="float: right; margin-top:2%; text-decoration: none;">Berita</a>
-    <a href="Product.php" class="w3-bar-item" style="float: right; margin-top:2%; text-decoration: none;">Produk</a>
-    <a href="Promo.php" class="w3-bar-item" style="float: right; margin-top:2%; text-decoration: none;">Promo</a>
-    <a href="Services.php" class="w3-bar-item" style="float: right; margin-top:2%; text-decoration: none;">Servis</a>
-    <a href="Book.php" class="w3-bar-item" style="float: right; margin-top:2%; text-decoration: none;">Book</a>
-  </div>
+<?php include_once './_partials/Navbar.php'; ?>
 
 <?php
     $querySelect = "SELECT namaJasa, hargaJasa FROM jasa";
